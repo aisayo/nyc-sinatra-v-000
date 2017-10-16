0f9c9defe6a23106a8ef747f0d93359ce0ffba68
aisayo <aisayo@icloud.com>
2017-10-16 02:17:38 +0000

    automatically backed up by learn

--- app/controllers/figure_controller.rb
+++ app/controllers/figure_controller.rb
@@ -16,7 +16,7 @@ class FiguresController < ApplicationController
     if params["landmark"]["name"].empty?
     @figure.landmarks << Landmark.create(params["landmark"])
     end
-    if params["title"]["name"].empty?
+    if !params["title"]["name"].empty?
       @figure.titles << Title.create(params["title"])
     end
     @figure.save
