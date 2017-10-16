e9a8db944f6d92d4e00f8c69713c029a9f20703e
aisayo <aisayo@icloud.com>
2017-10-16 02:32:49 +0000

    automatically backed up by learn

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -2,4 +2,19 @@ class Figure <ActiveRecord::Base
   has_many :landmarks
   has_many :figure_titles
   has_many :titles, through: :figure_titles
+
+  def slug
+    self.name.downcase.split(" ").join("-")
+  end
+
+  def self.find_by_slug(slug)
+    match = ""
+
+    self.all.each do |artist|
+      if artist.slug == slug
+         match = artist
+      end
+    end
+    match
+  end
 end
