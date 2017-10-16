f9b8ea3ca90c4f0cc356e2c177312be4c92c25f0
aisayo <aisayo@icloud.com>
2017-10-16 04:15:18 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -4,6 +4,11 @@ class LandmarksController < ApplicationController
     erb :'landmarks/new'
   end
 
+  post '/landmarks' do
+    @landmark = Landmark.create(params['landmark'])
+    redirect to "/landmarks/#{@landmark.id }"
+  end
+
   get '/landmarks' do
     @landmarks = Landmark.all
     erb :'landmarks/landmarklist'
