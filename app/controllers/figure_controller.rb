958d4eecd480d2c5fc2dfdb18674bd4fa0791f2c
aisayo <aisayo@icloud.com>
2017-10-16 00:37:55 +0000

    Done.

---
+++ app/controllers/figure_controller.rb
@@ -0,0 +1,18 @@
+class FiguresController < ApplicationController
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/figureslist'
+  end
+
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    puts params
+    @figures = Figure.create(:name => params[:id])
+    @figures.save
+  end
+
+end
