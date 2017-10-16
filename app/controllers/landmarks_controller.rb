053ba2f0e61dc0743ae0ac91491047c714a017f1
aisayo <aisayo@icloud.com>
2017-10-16 04:29:28 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -17,6 +17,7 @@ class LandmarksController < ApplicationController
   get '/landmarks/:id/edit' do
     @landmark = Landmark.find_by_id(params[:id])
     erb :'/landmarks/edit'
+    redirect to "/landmarks/#{@landmark.id}"
   end
 
 end
