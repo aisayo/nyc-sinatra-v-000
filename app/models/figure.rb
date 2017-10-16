c915de0873432d94fe3bd110bfdd0b27c6c657bd
aisayo <aisayo@icloud.com>
2017-10-16 02:33:48 +0000

    automatically backed up by learn

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -2,19 +2,4 @@ class Figure <ActiveRecord::Base
   has_many :landmarks
   has_many :figure_titles
   has_many :titles, through: :figure_titles
-
-  def slug
-    self.name.downcase.split(" ").join("-")
-  end
-
-  def self.find_by_slug(slug)
-    match = ""
-
-    self.all.each do |figure|
-      if figure.slug == slug
-         match = figure
-      end
-    end
-    match
-  end
 end
